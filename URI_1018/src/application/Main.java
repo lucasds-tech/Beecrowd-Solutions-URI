7ffe8b7b956bae84dbb6c5ff1a3f7827dac9cf20
Lucas da Silva <lucas.dasilva09@hotmail.com>
2024-12-28 10:12:14 -0300

    Modifed and Upload URI 1018

---
+++ URI_1018/src/application/Main.java
@@ -0,0 +1,39 @@
+package application;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        
+		int notasInseridas = sc.nextInt();
+        int att = notasInseridas;
+        
+        int notas100 = att / 100;
+        att -= notas100 * 100;
+        int notas50 = att / 50;
+        att -= notas50 * 50;
+        int notas20 = att / 20;
+        att -= notas20 * 20;
+        int notas10 = att / 10;
+        att -= notas10 * 10;
+        int notas5 = att / 5;
+        att -= notas5 * 5;
+        int notas2 = att / 2;
+        att -= notas2 * 2;
+        int notas1 = att;
+        
+        System.out.println(notasInseridas);
+        System.out.println(notas100 + " nota(s) de R$ 100,00");
+        System.out.println(notas50 + " nota(s) de R$ 50,00");
+        System.out.println(notas20 + " nota(s) de R$ 20,00");
+        System.out.println(notas10 + " nota(s) de R$ 10,00");
+        System.out.println(notas5 + " nota(s) de R$ 5,00");
+        System.out.println(notas2 + " nota(s) de R$ 2,00");
+        System.out.println(notas1 + " nota(s) de R$ 1,00");
+        
+        sc.close();
+	}
+
+}
