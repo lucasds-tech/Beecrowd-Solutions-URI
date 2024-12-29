b63f350540cf06e6a05a257e605ce8a242524e2a
Lucas da Silva <lucas.dasilva09@hotmail.com>
2024-12-29 18:20:27 -0300

    Modifed and Upload URI 1018

--- URI_1018/src/application/Main.java
+++ URI_1018/src/application/Main.java
@@ -1,3 +1,10 @@
+/*
+ * Leia um valor inteiro. A seguir, calcule o menor número de 
+ * notas possíveis (cédulas) no qual o valor pode ser decomposto.
+ * As notas consideradas são de 100, 50, 20, 10, 5, 2 e 1. 
+ * A seguir mostre o valor lido e a relação de notas necessárias.
+ */
+
 package application;
 
 import java.util.Scanner;
